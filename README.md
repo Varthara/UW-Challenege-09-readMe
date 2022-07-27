204451f98063db39be772e29c69bfc83571d5bca
Zane Sharif <Zane.Sharif@gmail.com>
2022-07-27 12:58:39 -0700

    refined inputs, complete

---
+++ README.md
@@ -0,0 +1,23 @@
+# ReadMe Generator
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+ReadMe Generator for projects
+## Table of Contents
+1. [Installation](#installation)
+2. [Usage](#usage)
+3. [Licenses](#licenses)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+## Installation
+Installation instructions: No instructions.
+## Usage
+None
+## Licenses
+This project is licensed under the MIT license.
+## Contributing
+NA.
+## Tests
+NA
+## Questions
+Questions can be directed to [email](zane.sharif@gmail.com). For the developer's Github please refer to: [Github](https://github.com/https://github.com/Varthara) or [LinkedIn](https://linkedin.com/in/https://www.linkedin.com/in/zane-sharif-486b15152/).
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
+// Global variables for packages
 const inquirer = require('inquirer');
 const fs = require('fs');
 let licenseBadge = '';
+// retrieves badges based on answer from .prompt
+const badge = (answers) => {
+  if (answers.license === 'MIT') {
+      licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    console.log(answers.license)
+  } else if (answers.license === 'GPL'){
+      licenseBadge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+    console.log(answers.license)
+  } else if (answers.license === 'Apache'){
+      licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    console.log(answers.license)
+  } else {
+      licenseBadge = "No license selected";
+  }
+}
 
 
-
+// Template to generate readMe
 const generateReadMe = (answers) => {
   badge(answers);
   fs.writeFile('./README.md', 
@@ -19,26 +35,40 @@ ${answers.description}
 5. [Tests](#tests)
 6. [Questions](#questions)
 ## Installation
-A link to the deployed site can be found [here](${answers.run}). A screenshot of the site is shown below: 
-![screenshot of deployed site for ${answers.name}](${answers.url})
+Installation instructions: ${answers.install}.
 ## Usage
 ${answers.usage}
 ## Licenses
-This project uses the ${answers.license} license. More info about licenses can be found [here](https://choosealicense.com/).
+This project is licensed under the ${answers.license} license.
 ## Contributing
-This project is open for contribution but please initiate an issue for any significant changes. 
+${answers.contribute}.
 ## Tests
 ${answers.tests}
 ## Questions
-The developer can be reached through [email](${answers.email}) or contacted through [Github](https://github.com/${answers.github}) or [LinkedIn](https://linkedin.com/in/${answers.linkedin}).`,
-(err) => err ? console.log(err) : console.log('nice'))}
-
+Questions can be directed to [email](${answers.email}). For the developer's Github please refer to: [Github](https://github.com/${answers.github}) or [LinkedIn](https://linkedin.com/in/${answers.linkedin}).`,
+(err) => err ? console.log(err) : console.log('Success'))}
+// Inquirer questions to recieve user input. 
 inquirer
   .prompt([
     {
       type: 'input',
       name: 'name',
       message: 'What is the project name?'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter a link to your Github account?'
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'Please enter a link to you Linkedin account?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'Please enter your email address.'
     },
     {
       type: 'input',
@@ -65,10 +95,11 @@ inquirer
       name: 'tests',
       message: 'Please enter test instructions',
     },
-  ])
-  .then((data) => {
-    const content = generateHTML(answers);
-
-    fs.writeFile('index.html', content, (err) => err ? console.log(err) : console.log('Success!')
-    );
-  });
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Please choose your desired liscense.',
+      choices: ['MIT', 'GPL', 'Apache'],
+    },
+  ]).then((answers) => {generateReadMe(answers);
+  })
